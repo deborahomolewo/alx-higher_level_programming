9400aa78334e4aedec672c26395392207efe3b1e
Deborah <omolewod@gmail.com>
2022-11-29 06:24:05 +0100

    Task 6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -2,8 +2,4 @@
 str1 = "Holberton"
 str2 = "School"
 str1 = "{} {}".format(str1, str2)
-<<<<<<< HEAD
 print("Welcome to {}!".format(str1))
-=======
-print("Welcome to {}!".format(str1)) 
->>>>>>> f90f5865eceec3870a5667b211febea9878eecec
